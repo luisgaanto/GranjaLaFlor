69f373a41cefb0a2a66db0ab81debb35a7a393e7
luisgaanto <luisgaanto@gmail.com>
2026-06-09 07:29:59 -0700

    Create NugetsNotes.cs
    Adding Nugets and notes

---
+++ Project_Data_Progress_Notes/NugetsNotes.cs
@@ -0,0 +1,29 @@
+
+//Nugets notes for Project Granja la Flor. 
+
+
+/*
+
+    Microsoft.EntityFrameworkCore:
+
+Main EF Core library. Gives you DbContext, DbSet, LINQ queries, SaveChanges(), relationships, 
+validations, etc.
+
+    Microsoft.EntityFrameworkCore.Tools:
+
+Allows EF Core commands like Add-Migration, Update-Database, dotnet ef migrations add, 
+and dotnet ef database update.
+
+    Microsoft.EntityFrameworkCore.SqlServer:
+
+Provider for SQL Server. Use this only if your database is SQL Server. SQL Server connection
+
+    Pomelo.EntityFrameworkCore.MySql:
+
+Provider for MySQL/MariaDB. Use this if Granja La Flor uses MySQL.
+Is one of the most used for MySQL connection
+
+    Microsoft.VisualStudio.Web.CodeGeneration.Design:
+
+
+*/
